27a8c7c690c0b7c9268b36c785a759a76ce81af5
ahmed-dadousha <bioshock44matrix44@gmail.com>
2023-04-24 10:53:50 +0200

    Feat: make sxhkd run on startup

--- keyboard_8.sh
+++ keyboard_8.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-# Create .config/sxhkd/sxhkdrc
-printf "\n======================================================================= Create sxhkd config file ================================================================================\n"
+# Config SXHKD
+printf "\n==================================================================== Config SXHKD ==============================================================================================\n"
 mkdir ~/.config/sxhkd && touch ~/.config/sxhkd/sxhkdrc
+mkdir -p ~/.config/systemd/user/
+
+
+# 
+CONFIG="[Unit]
+Description=Simple X Hotkey Daemon
+After=xsession.target
+
+[Service]
+ExecStart=/usr/bin/sxhkd -c ~/.config/sxhkd/sxhkdrc
+Restart=always
+User=ahmed
+
+[Install]
+WantedBy=default.target"
+
+# Reload the SystemD daemon to read the new unit file by running the following command:
+systemctl --user daemon-reload
+# Enable the sxhkd service to start on login by running the following command:
+systemctl --user enable sxhkd.service
 
 # Add keyboard shortcuts
 # Terminal, Browser, Sound, Mount, File manger, Screenshot, Exit, Screencast
@@ -18,4 +38,4 @@ EOF
 
 # Make sxhkd run on system startup
 
-printf "\n================================================================================= Done ==========================================================================================\n"
+printf "\n================================================================================= Done =========================================================================================\n"
